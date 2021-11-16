95bd47e698c3104a6d40fdfbd2d938340bf6553d
jaafar <jaafar.habu@gmail.com>
2021-11-16 11:47:10 +0100

    added footer and moved nav to header div

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,5 +1,10 @@
-</div>
-                  
+                        
+                    </div>
+                        <div class="footer">
+                                <footer>
+                                    <p> Jafsoft &copy; 2021 - <?php echo date("Y")." </p>"?>
+                                </footer>
+                        </div>
                   </div>
               </main>
               <script src='/mytwyt/js/twyt.js'></script>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -11,30 +11,35 @@
         <body>
             <main>
                 <div class='main-content-wrapper'>
-                 <nav>
-                    <span>mytwyt</span>
-                    <ul>
-                        <li <?php echo $activeArray['home'];?>>
-                            <a href="/mytwyt/index.php">Home|Latest</a>
-                        </li>
-                        <li <?php echo $activeArray['nigerian-news'];?>>
-                            <a href="/mytwyt/nigerian-news/index.php">Nigerian-News</a>
-                        </li>
-                        <li <?php echo $activeArray['web-dev'];?>>
-                            <a href="/mytwyt/web-dev/index.php">Web Dev</a>
-                        </li>
-                        <li <?php echo $activeArray['flutter'];?>>
-                            <a href="/mytwyt/flutter/index.php">Flutter</a>
-                        </li>
-                        <li <?php echo $activeArray['technology'];?>>
-                            <a href="/mytwyt/technology/index.php">Technology</a>
-                        </li>
-                        <li <?php echo $activeArray['football'];?>>
-                            <a href="/mytwyt/football/index.php">Football</a>
-                        </li>
-                        <li <?php echo $activeArray['favorites'];?>>
-                        <a href="/mytwyt/favorites/index.php">Favorites</a>
-                        </li>
-                    </ul>
-                 </nav>
+                    <div class="header">
+                       
+                        <span>mytwyt</span>
+                        <nav>
+                            
+                            <ul>
+                                <li <?php echo $activeArray['home'];?>>
+                                    <a href="/mytwyt/index.php">Home|Latest</a>
+                                </li>
+                                <li <?php echo $activeArray['nigerian-news'];?>>
+                                    <a href="/mytwyt/nigerian-news/index.php">Nigerian-News</a>
+                                </li>
+                                <li <?php echo $activeArray['web-dev'];?>>
+                                    <a href="/mytwyt/web-dev/index.php">Web Dev</a>
+                                </li>
+                                <li <?php echo $activeArray['flutter'];?>>
+                                    <a href="/mytwyt/flutter/index.php">Flutter</a>
+                                </li>
+                                <li <?php echo $activeArray['technology'];?>>
+                                    <a href="/mytwyt/technology/index.php">Technology</a>
+                                </li>
+                                <li <?php echo $activeArray['football'];?>>
+                                    <a href="/mytwyt/football/index.php">Football</a>
+                                </li>
+                                <li <?php echo $activeArray['favorites'];?>>
+                                <a href="/mytwyt/favorites/index.php">Favorites</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                 
                     <div class='twyt-list-wrapper'>
