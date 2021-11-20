0d94d6fc1e7135a323d1f40ee15afd62862f3e6f
jaafar <jaafar.habu@gmail.com>
2021-11-20 13:34:46 +0100

    added connection obj and getconnection method

--- favorites/deleteFavorite.php
+++ favorites/deleteFavorite.php
@@ -2,5 +2,6 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/controllers/twytController.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/twytService.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/connection.php';
-$twytController = new TwytController(Connection::getConnection());
+$twytConnection = new  Connection;
+$twytController = new TwytController($twytConnection->getConnection());
 $twytController->deleteFavorite($_POST['twytId']);
