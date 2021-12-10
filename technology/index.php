9a7abdd97b71c7b559782af04c7746acaeccebdf
jaafar <jaafar.habu@gmail.com>
2021-12-10 02:20:04 +0100

    added try catch in technology status

--- technology/index.php
+++ technology/index.php
@@ -10,6 +10,7 @@
     "search" => ""
 );?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/mytwyt/includes/header.inc.php';?>
+
 <?php 
 date_default_timezone_set("Africa/Lagos");
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/connection.php';
@@ -17,10 +18,16 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/views/twytViews.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/twytService.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/dbService.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/controllers/twytController.php';
-$twytService = new TwytService(new DbService());
-$twytController = new TwytController(new DbService(), new TwytService(new DbService()),new Connection());
-$twytView = new TwytView($twytController);
-if ($twytService->createListStatusJson(203356824,'technology')!=null)$twytService->createListStatusJson(203356824,'technology');
-echo $twytView->view($twytController->getTechnologyJson(),false);
+
+try {
+
+    $twytService = new TwytService(new DbService());
+    $twytController = new TwytController(new DbService(), new TwytService(new DbService()),new Connection());
+    $twytView = new TwytView($twytController);
+    if ($twytService->createListStatusJson(203356824,'technology')!=null)$twytService->createListStatusJson(203356824,'technology');
+    echo $twytView->view($twytController->getTechnologyJson(),false);
+} catch (\Throwable $e) {
+   echo $e->getMessage();
+}
 ?>
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/mytwyt/includes/footer.inc.php';?>
