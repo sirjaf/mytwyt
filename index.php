fc36b0df9ba3b9ab78d6d8f5837f94cc0b618c98
jaafar <jaafar.habu@gmail.com>
2021-12-10 03:01:12 +0100

    added try-catch to main index files

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php $activeArray = array(
-    "home"=>"class='active'",
-    "nigerian-news"=>"",
-    "web-dev"=>"",
-    "flutter"=>"",
-    "technology"=>"",
-    "android-news"=>"",
-    "football"=>"",
-    "favorites"=>"",
+    "home" => "class='active'",
+    "nigerian-news" => "",
+    "web-dev" => "",
+    "flutter" => "",
+    "technology" => "",
+    "android-news" => "",
+    "football" => "",
+    "favorites" => "",
     "search" => ""
-);?>
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/mytwyt/includes/header.inc.php';?>
+); ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/includes/header.inc.php'; ?>
 <?php
-date_default_timezone_set("Africa/Lagos");
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/connection.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/views/twytViews.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/twytService.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/dbService.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/controllers/twytController.php';
-$twytService = new TwytService(new DbService());
-$twytController = new TwytController(new DbService(), new TwytService(new DbService()),new Connection());
-$twytView = new TwytView($twytController);
-if ($twytService->createHomeTimelineJson()!==null)$twytService->createHomeTimelineJson();
-echo $twytView->homeTimelimeView();
+try {
+    date_default_timezone_set("Africa/Lagos");
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/connection.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/views/twytViews.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/twytService.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/dbService.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/controllers/twytController.php';
+    $twytService = new TwytService(new DbService());
+    $twytController = new TwytController(new DbService(), new TwytService(new DbService()), new Connection());
+    $twytView = new TwytView($twytController);
+    if ($twytService->createHomeTimelineJson() !== null) $twytService->createHomeTimelineJson();
+    echo $twytView->homeTimelimeView();
+} catch (\Throwable $e) {
+    echo $e->getMessage();
+}
 ?>
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/mytwyt/includes/footer.inc.php';?>
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/includes/footer.inc.php'; ?>
