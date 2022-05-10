a3bc70658274458f80acf47f5434b23732b7f3c9
jaafar <jaafar.habu@gmail.com>
2022-05-10 02:01:35 +0100

    added createFavoriteJson for creating json for favorite after adding and deleting favorite

--- favorites/addFavorite.php
+++ favorites/addFavorite.php
@@ -4,5 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/twytService.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/dbService.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/connection.php';
 // $twytConnection = new  Connection; 
+$twytService = new TwytService(new DbService());
 $twytController = new TwytController(new DbService(), new TwytService(new DbService()),new Connection());
 $twytController->addFavorite($_POST['twytId'],$_POST['twytText'],$_POST['twytUserScreenName'], $_POST['twytUrl'],$_POST['twytCreatedAt'],$_POST['twytProfileImage']);
+$twytService->createFavoritesJson();
--- favorites/deleteFavorite.php
+++ favorites/deleteFavorite.php
@@ -3,5 +3,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/controllers/twytController.php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/services/twytService.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/mytwyt/connection.php';
 // $twytConnection = new  Connection;
+$twytService = new TwytService(new DbService());
 $twytController = new TwytController(new DbService(), new TwytService(new DbService()),new Connection());
 $twytController->deleteFavorite($_POST['twytId']);
+$twytService->createFavoritesJson();
